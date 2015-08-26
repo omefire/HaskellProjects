c9bf5e40d1ce5a5fb8faf615c9e231b58f5b920e
Omar Mefire <omefire@gmail.com>
2015-08-25 23:28:48 -0700

    Wrapping Maybe within an IORef

---
+++ ioref-Maybe.hs
@@ -0,0 +1,21 @@
+import Data.IORef
+
+magic :: IORef (Maybe Int) -> IO ()
+magic ref = do
+  value <- readIORef ref
+
+  case value of
+   Just _ -> return ()
+   Nothing -> writeIORef ref (Just 42)
+
+
+main :: IO ()
+main = do
+  ref <- newIORef (Nothing)
+  magic ref
+  val <- readIORef ref
+
+  case val of
+    Just x -> print x
+    Nothing -> print "Empty"
+  
