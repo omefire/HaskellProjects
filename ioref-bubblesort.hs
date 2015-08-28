f3cee1c813c93a455a9df039ee039a3f4dea60cb
Omar Mefire <omefire@gmail.com>
2015-08-27 22:08:24 -0700

    Bubble Sort using IORef

--- ioref-bubblesort.hs
+++ ioref-bubblesort.hs
@@ -1,4 +1,31 @@
+import Data.IORef
+import Data.Foldable
+import Control.Monad
+
 bubbleSort :: [Int] -> IO [Int]
 bubbleSort input = do
   let n = length input
-  
+
+  xs <- mapM newIORef input
+  forM_ [0..n-1] $ \i -> do
+    forM_ [0..n-2] $ \j -> do
+      let ix = xs !! j
+      let iy = xs !! (j + 1)
+
+      x <- readIORef ix
+      y <- readIORef iy
+
+      when (x > y) $ do
+        writeIORef ix y
+        writeIORef iy x
+
+  mapM readIORef xs
+
+
+main :: IO ()
+main = do
+  let xs = [9, 2, 15, 7]
+  sorted_xs <- bubbleSort xs
+  print sorted_xs
+    
+    
